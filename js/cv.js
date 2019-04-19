353b00241c6cba5884cabef135f56bf066b4334d
Daniel Maria da Silva <danielmariadasilva@gmail.com>
2019-04-19 19:55:50 -0300

    Alterações do CV.

---
+++ js/cv.js
@@ -0,0 +1,96 @@
+var cursosList = [
+    {title:"Android I: Crie sua App fantástica no Eclipse", date: new Date("03/22/2016"), duracao:720, showCV: false, showResume: false, company: "Alura.com"},
+    {title:"Jogos com Android: Desenvolva seu próprio Flappy Bird", date: new Date("04/08/2016"), duracao:480, showCV: false, showResume: false, company: "Alura.com"},
+    {title:"Jogos com Android I: Desenvolvimento de jogos 2D", date: new Date("03/31/2016"), duracao:480, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Android I: Crie sua App fantástica com Android Studio", date: new Date("03/11/2016"), duracao:600, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Design Patterns Java I: Boas práticas de programação", date: new Date("10/29/2015"), duracao:1200, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Eclipse: Produtividade Extrema na IDE com Java", date: new Date("11/18/2015"), duracao:720, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Orientação a Objetos: Melhores técnicas com Java", date: new Date("03/01/2016"), duracao:1200, showCV: false, showResume: false, company: "Alura.com"},
+    {title:"Testes: TDD com Java", date: new Date("02/23/2016"), duracao:720, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Java I: Primeiros passos", date: new Date("08/25/2015"), duracao:720, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Java e JDBC: Trabalhando com um banco de dados", date: new Date("10/15/2015"), duracao:480, showCV: false, showResume: false, company: "Alura.com"},
+    {title:"Refatorando na prática", date: new Date("11/17/2015"), duracao:480, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Java II: Orientação a Objetos", date: new Date("08/28/2015"), duracao:720, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Java III: Principais APIs e bibliotecas", date: new Date("09/30/2015"), duracao:720, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Java 8: Tire proveito dos novos recursos da linguagem", date: new Date("11/13/2015"), duracao:720, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Clojure: Introdução a programação funcional", date: new Date("11/30/2015"), duracao:480, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Threads I : Programação paralela em Java", date: new Date("05/18/2016"), duracao:480, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Java: Dominando as Collections", date: new Date("08/01/2016"), duracao:1200, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"JavaScript : Programando na linguagem da web", date: new Date("09/04/2015"), duracao:1200, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"HTML5 e CSS3 I: Suas primeiras páginas da Web", date: new Date("09/01/2015"), duracao:1920, showCV: false, showResume: false, company: "Alura.com"},
+    {title:"HTML5 e CSS3 II: Turbinando as suas páginas", date: new Date("10/15/2015"), duracao:2400, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Métodos Ágeis: Introdução", date: new Date("11/13/2015"), duracao:720, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"UML Introdução: Modelagem de soluçõe", date: new Date("08/24/2015"), duracao: 480, showCV: false, showResume: false, company: "Alura.com"},
+    {title:"Scrum: Agilidade em seu projeto", date: new Date("01/14/2016"), duracao:600, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Lean Startup: Primeiros passos da sua startup enxuta", date: new Date("10/26/2015"), duracao:60, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"MySQL II: Consultas poderosas", date: new Date("11/18/2015"), duracao:960, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"MySQL I: Iniciando suas consultas", date: new Date("09/12/2015"), duracao:480, showCV: false, showResume: false, company: "Alura.com"},
+    {title:"Oracle I: Aprenda SQL usando esse famoso banco de dados", date: new Date("12/01/2015"), duracao:480, showCV: false, showResume: false, company: "Alura.com"},
+    {title:"Redis I: Armazenando chaves e valores", date: new Date("11/25/2015"), duracao:1200, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Redis II: Estruturas e recursos na sua base NoSQL", date: new Date("11/27/2015"), duracao:600, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"SQLServer I: desvendando o banco de dados", date: new Date("12/01/2015"), duracao:540, showCV: false, showResume: false, company: "Alura.com"},
+    {title:"SQLServer II: Consultas poderosas", date: new Date("12/04/2015"), duracao:600, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Oracle II: Consultas Complexas", date: new Date("02/05/2016"), duracao:480, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"SQL Completo", date: new Date("08/05/2015"), duracao:1200, showCV: false, showResume: false, company: "Softblue"},
+    {title:"Desenvolvedor NodeJS e MongoDB", date: new Date("07/27/2017"), duracao:930, showCV: true, showResume: false,  company: "Udemy.com"},
+    {title:"Git Completo: Do Básico ao Avançado", date: new Date("07/27/2017"), duracao:120, showCV: true, showResume: false,  company: "Udemy.com"},
+    {title:"Criar aplicativos com Ionic 3, Spring, OAuth2, REST e MongoDB", date: new Date("12/12/2018"), duracao:300, showCV: true, showResume: false,  company: "Udemy.com"},
+    {title:"Design Thinking com Crianças e Jovens: Aplicação e Exemplos", date: new Date("04/02/2019"), duracao:42, showCV: true, showResume: false,  company: "Udemy.com"},
+    {title:"API RESTful com Spring Boot e Java 8", date: new Date("04/06/2019"), duracao:930, showCV: true, showResume: false,  company: "Udemy.com"},
+    {title:"Testes unitários em JAVA: Domine JUnit, Mockito e TDD", date: new Date("04/17/2019"), duracao:480, showCV: true, showResume: false,  company: "Udemy.com"},
+    {title:"Treinamento Scrum Developer", date: new Date("01/01/2018"), duracao:480, showCV: true, showResume: false,  company: "DBCCompany"}, //
+    {title:"Certificado CLF (Certified Lean Inception Facilitator", date: new Date("08/01/2018"), duracao:480, showCV: true, showResume: false, company: "Caroli.org"},
+    {title:"Introdução à Business Intelligence e Data Warehouse", date: new Date("02/14/2018"), duracao:300, showCV: true, showResume: false, company: "BI do Brasil"}
+  ];
+
+  Array.prototype.sumIgnoring = function (prop, ignore) {
+    var total = 0
+    for ( var i = 0, _len = this.length; i < _len; i++ ) {
+        if(!this[i][ignore]){
+            total += this[i][prop]
+        }
+    }
+    return total
+}
+function getSortedCursos(){ 
+    return cursosList.sort(function(a, b) {
+            dateOne = new Date(a.date);
+            dateTwo = new Date(b.date);
+            return dateOne>dateTwo ? -1 : dateOne<dateTwo ? 1 : 
+                a.title>b.title ? 1 : a.title<b.title ? -1 : 0;
+        });
+}
+
+function printClasses(ignoreVar){
+    var htmlCursos = "<ul>";
+    getSortedCursos().forEach(element => {
+        if(ignoreVar || element.showCV){
+            htmlCursos += "<li>";
+            htmlCursos += element.title + ' (' + element.company + ', ' + element.date.getFullYear() + ');';
+            htmlCursos += "</li>";
+        }
+    });
+    htmlCursos += "</ul>";
+    document.getElementById("cursos-qualificacoes").innerHTML = htmlCursos;
+    if(!ignoreVar){
+        let tempoCurso = cursosList.sumIgnoring('duracao', 'showCV');
+        document.getElementById("cursos-qualificacoes").innerHTML += "<p style='text-align: right;'> E mais " + tempoCurso/60 + " horas em outros cursos.</p>";
+    } else {
+        let tempoCurso = cursosList.sumIgnoring('duracao', '');
+        document.getElementById("cursos-qualificacoes").innerHTML += "<p style='text-align: right;'> Total de " + tempoCurso/60 + " horas.</p>";
+    }
+}
+
+function showAllClasses(){
+    printClasses(true);
+    document.getElementById("btnShowAllClasses").hidden = true;
+}
+
+(function() {
+    printClasses(false);
+    document.getElementById("yearNow").innerHTML = new Date().getFullYear();
+ })();
+
+
+
+
--- js/init.js
+++ js/init.js
@@ -17,7 +17,7 @@ $("#emailBtn").click(function(){
 });
 
 function putTextInLabels(){
-  let apresentation, time, theRoadSoFarTitle, theRoadSoFar, aboutMeTitle, aboutMe, myCourseTitle, myCourse, downloadText;
+  let apresentation, time, theRoadSoFarTitle, theRoadSoFar, aboutMeTitle, aboutMe;
   if(lang === "PT"){
     apresentation = "OI, ME CHAMO DANIEL & SOU";
     time = " de experiência, desenvolvendo, estudando e aprimorando a forma de criar soluções.";
@@ -25,9 +25,6 @@ function putTextInLabels(){
     theRoadSoFar = "Em 2015 me formei bacharel em educação física pela PUCRS. Tive a certeza de que esta, apesar de ser uma área incrível, não era minha paixão. Enquanto estudava sobre agachamento e bioquímica, realizava cursos de programação. Iniciei em 2016 o curso de Sistemas de Informação e, após 3 meses fui contratado como desenvolvedor.";
     aboutMeTitle = "Sobre mim";
     aboutMe = "Eu sou um backend por profissão e um frontend por hobby. Sou completamente viciado em desenvolvimento de software. Em meu dia a dia, profissionalmente utilizo Java (JSF, RichFaces, JSP, Servlets, Hibernate, Spring) e Oracle como banco de dados. Apesar disto, muitas outras tecnologias me agradam. Como diversão gosto de aprender sobre outras tecnologias, como NodeJS, Vanilla JavaScript, JQuery, MongoDB, etc.";
-    myCourseTitle = "Iniciando como programador com JAVA";
-    myCourse = "O curso está em processo de criação. É um projeto feito por mim com o intuito de disseminar conhecimento para novos programadores, onde o aluno poderá iniciar seus estudos sem nenhum conhecimento prévio e ter uma noção do que é programação, passando desde os conhecimentos básicos de operadores aritiméticos até testes unitários e orientação a objeto. Em forma de retribuição aos milhares de criadores de conteúdo na internet o curso será sempre gratuito.";
-    downloadText = "Se você quiser, você pode fazer o download do meu curriculum vitae ou resume:";
     $('.loopWordsEnglish').hide();
     $('.loopWordsPortuguese').show();
   } else {
@@ -37,9 +34,6 @@ function putTextInLabels(){
     theRoadSoFar = "In 2015 I undergraduated Bachelor in Physical Education at PUCRS. I was sure that this area was incredible, despite I still interested about it, I wanted to do something different. While I studied about squat and biochemistry, I made some courses on programming. So, in 2016 I started Information Systems course at University (it's kind of mixed of administration and thecnology in Brazil). Three moths later, I was hired to work with programming.";
     aboutMeTitle = "About Me";
     aboutMe = "I am a backend in my profession and a frontend by hobby. I am completly addicted in software development. Daily in my job, I use Java (JSF, RichFaces, JSP, Servlets, Hibernate, Spring) and Oracle for database. Despite this, I like a lot of others technologies. For fun, I like to learn NodeJS, Vanilla JavaScript, JQuery, MongoDB, etc.";
-    myCourseTitle = "Starting as a developer with JAVA";
-    myCourse = "The course is in construction process. It is a project made by myself, aiming to multiply knowledge for new programmers. The student will be able to start their studies without any previously knowledge in programming, since the basic knologie, from the basic knowledge of arithmetic operators to unit tests and object orientation. In order to reward the thousands creators of content in the internet, the course will always be free.";
-    downloadText = "It is only in Portuguese, but if you want, you can make a download of my curriculum vitae or resume:";
     $('.loopWordsEnglish').show();
     $('.loopWordsPortuguese').hide();
   }
@@ -49,9 +43,6 @@ function putTextInLabels(){
   document.getElementById("theRoadSoFar").innerHTML = theRoadSoFar;
   document.getElementById("aboutMe").innerHTML = aboutMe;
   document.getElementById("aboutMeTitle").innerHTML = aboutMeTitle;
-  document.getElementById("myCourseTitle").innerHTML = myCourseTitle;
-  document.getElementById("myCourse").innerHTML = myCourse;
-  document.getElementById("downloadText").innerHTML = downloadText;
 }
 
 function contTimeExperience(){
@@ -141,206 +132,7 @@ window.onload = function() {
     document.body.appendChild(css);
 };
 
-function generateResume(){ 
-  var h = 30; 
-  var doc = new jsPDF(); 
-  var idade = Math.abs(new Date(Date.now() - new Date('02/10/1993').getTime()).getUTCFullYear() - 1970); 
-  doc.addFont('ArialMS', 'Arial', 'normal'); 
-  doc.setFont('Arial'); 
- 
-  doc.setFontSize(25); 
-  doc.text("Daniel Maria da Silva", 23, h+=5); 
- 
-  doc.setFontSize(9); 
-  doc.text("Brasileiro, Solteiro, " + idade + " anos", 25, h+=5); 
-  doc.text("Avenida Ipiranga, 7020", 25, h+=5); 
-  doc.text("Jardim Botânico – Porto Alegre – Rio Grande do Sul", 25, h+=5); 
-  doc.text("Telefone: 51 99314-1835 – E-mail: danielmariadasilva@gmail.com", 25, h+=5); 
- 
-  doc.setFontSize(13); 
-  doc.text("RESUMO", 23, h+=10); 
-  doc.rect(23, h, 165, 0.1, 'F'); 
-  doc.setFontSize(10); 
-  doc.text("Trabalho profissionalmente com programação de software desde 2016, utilizando diariamente tecnologias" ,25, h+=5); 
-  doc.text("Java, Spring MVC, JPA, JSF, Hibernate, Oracle. Busco o aperfeiçoamento de meus conhecimentos, bem", 25, h+=5); 
-  doc.text("como a multiplicação dela ao trocá-la com meus colegas de trabalho.", 25, h+=5); 
- 
-  doc.setFontSize(13); 
-  doc.text("FORMAÇÃO", 23, h+=10); 
-  doc.rect(23, h, 165, 0.1, 'F'); 
-  doc.setFontSize(10); 
-  doc.text("Sistema de informação – PUCRS (2016 - cursando)", 25, h+=5); 
-  doc.text("Bacharelado em Educação Física e Ciências do Desporto - PUCRS (concluído 2015).", 25, h+=5); 
- 
-  doc.setFontSize(13); 
-  doc.text("EXPERIÊNCIA PROFISSIONAL", 23, h+=10); 
-  doc.rect(23, h, 165, 0.1, 'F'); 
-  doc.setFontSize(10); 
-  doc.text("06/16 - 02/18 – Gerência da Tecnologia da Informação e Telecomunicação - PUCRS", 25, h+=5); 
-  doc.text("Cargo: Programador", 25, h+=5); 
-  doc.setFontSize(10); 
-  doc.text("Principais atividades: Realizar manutenção e evolução dos sistemas já existentes, além de elaboração e " ,25, h+=5); 
-  doc.text("criação de novos. Utilizando principalmente Java como linguagem de programação back-end e os  " ,25, h+=5); 
-  doc.text("frameworks JSP, Servlet, Spring MVC, Hibernate. Como padrão era utilizado o banco de dados Oracle.",25, h+=5); 
 
-  doc.text("02/18 - atual – DBC Company", 25, h+=10); 
-  doc.text("Cargo: Programador", 25, h+=5); 
-  doc.setFontSize(10); 
-  doc.text("Principais atividades: Realizar a construção de sistemas utilizando Java como linguagem de programação" ,25, h+=5); 
-  doc.text("back-end e frameworks Primefaces e Hibernate, além de realizar as customizações necessárias utilizando " ,25, h+=5); 
-  doc.text("Bootstrap, JavaScript e JQuery.",25, h+=5); 
-  
- 
-  doc.setFontSize(13); 
-  doc.text("QUALIFICAÇÕES E ATIVIDADES PROFISSIONAIS", 23, h+=10); 
-  doc.rect(23, h, 165, 0.1, 'F'); 
-  doc.setFontSize(10); 
-  //doc.text("- HTML5 e CSS3 I: Suas primeiras páginas da Web (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- HTML5 e CSS3 II: Turbinando as suas páginas (Alura.com, 2015);", 25, h+=5); 
- // doc.text("- Java I: Primeiros passos (Alura.com, 2015);", 25, h+=5); 
- // doc.text("- Java II: Orientação a Objetos (Alura.com, 2015);", 25, h+=5); 
- // doc.text("- Java III: Principais APIs e bibliotecas (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Java 8: Tire proveito dos novos recursos da linguagem (Alura.com, 2015);", 25, h+=5); 
-  //doc.text("- Java: Dominando as Collections (Alura.com, 2016);", 25, h+=5); 
-  // doc.text("- Java e JDBC: Trabalhando com um banco de dados (Alura.com, 2015);", 25, h+=5); 
-  // doc.text("- Eclipse: Produtividade Extrema na IDE com Java (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Design Patterns Java I: Boas práticas de programação (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- SQL Completo (Softblue, 2015);", 25, h+=5); 
- // doc.text("- MySQL I: Iniciando suas consultas (Alura.com, 2015);", 25, h+=5); 
- // doc.text("- MySQL II: Consultas poderosas (Alura.com, 2015);", 25, h+=5); 
-  // doc.text("- Oracle I: Aprenda SQL usando esse famoso banco de dados (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Lean Startup: Primeiros passos da sua startup enxuta (Alura.com, 2015);", 25, h+=5); 
-  // doc.text("- Métodos Ágeis: Introdução (Alura.com, 2015);", 25, h+=5); 
-  // doc.text("- Refatorando na prática (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Clojure: Introdução a programação funcional (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- JavaScript : Programando na linguagem da web (Alura.com, 2015);", 25, h+=5); 
-  // doc.text("- Redis I: Armazenando chaves e valores (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Redis II: Estruturas e recursos na sua base NoSQL (Alura.com, 2015);", 25, h+=5); 
-  // doc.text("- SQLServer I: desvendando o banco de dados (Alura.com, 2015);", 25, h+=5); 
-  // doc.text("- SQLServer II: Consultas poderosas (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Oracle II: Consultas Complexas (Alura.com, 2016);", 25, h+=5);
-  doc.text("- UML: Uma introdução à modelagem  (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Android I: Crie sua App fantástica com Android Studio (Alura.com, 2016);", 25, h+=5); 
-  // doc.text("- Jogos com Android: Desenvolva seu próprio Flappy Bird (Alura.com, 2016);", 25, h+=5); 
-  doc.text("- Scrum: Agilidade em seu projeto (Alura.com, 2016);", 25, h+=5); 
-  // doc.text("- Android I: Crie sua App fantástica no Eclipse (Alura.com, 2016)", 25, h+=5); 
-  doc.text("- Jogos com Android I: Desenvolvimento de jogos 2D (Alura.com, 2016);", 25, h+=5); 
-  doc.text("- Testes: TDD com Java (Alura.com, 2016);", 25, h+=5); 
-  // doc.text("- Orientação a Objetos: Melhores técnicas com Java  (Alura.com, 2016)", 25, h+=5); 
-  doc.text("- Threads I : Programação paralela em Java  (Alura.com, 2016);", 25, h+=5); 
-  doc.text("- Desenvolvedor NodeJS e MongoDB. (Udemy, 2017);", 25, h+=5); 
-  doc.text("- Git Completo: Do Básico ao Avançado. (Udemy, 2017);", 25, h+=5); 
-  doc.text("- Introdução à Business Intelligence e Data Warehouse (BI do Brasil, 2018).", 25, h+=5); 
-  doc.text("- Treinamento Scrum Developer (DBCCompany, 2018);", 25, h+=5); 
-  doc.text("- Certificado CLF® (Certified Lean Inception Facilitator) – Lean Inception (Caroli.org, 2018);", 25, h+=5); 
-  doc.text("- Criar aplicativos com Ionic 3, Spring, OAuth2, REST e MongoDB (Udemy.com, 2018);", 25, h+=5); 
-  var today = new Date(); 
-  // doc.addPage(); 
-  // h = 30; 
-  today.setHours(0, 0, 0, 0); 
-  doc.text(today.toLocaleDateString(), 175, h+=5); 
-  doc.save('Resume_DanielMariaDaSilva.pdf'); 
-} 
- 
-function generateCV(){ 
-  var h = 30; 
-  var doc = new jsPDF(); 
-  var idade = Math.abs(new Date(Date.now() - new Date('02/10/1993').getTime()).getUTCFullYear() - 1970); 
-  doc.addFont('ArialMS', 'Arial', 'normal'); 
-  doc.setFont('Arial'); 
- 
-  doc.setFontSize(25); 
-  doc.text("Daniel Maria da Silva", 23, h+=5); 
- 
-  doc.setFontSize(9); 
-  doc.text("Brasileiro, Solteiro, " + idade + " anos", 25, h+=5); 
-  doc.text("Avenida Ipiranga, 7020", 25, h+=5); 
-  doc.text("Jardim Botânico – Porto Alegre – Rio Grande do Sul", 25, h+=5); 
-  doc.text("Telefone: 51 99314-1835 – E-mail: danielmariadasilva@gmail.com", 25, h+=5); 
- 
-  doc.setFontSize(13); 
-  doc.text("RESUMO", 23, h+=10); 
-  doc.rect(23, h, 165, 0.1, 'F'); 
-  doc.setFontSize(10); 
-  var resumo = doc.splitTextToSize("Trabalho profissionalmente com programação de software desde 2016, utilizando diariamente tecnologias Java, Spring, PrimeFaces, JPA, JSF, Hibernate, Oracle. Além disso, busco aprimorar meus conhecimentos em cursos relacionados a banco de dados NoSql e de tecnologias que fazem uso de JavaScript, como NodeJS, Ionic, etc. Procuro me aperfeiçoar em programação e em desenvolvimento de software, buscando o desenvolvimento de minhas habilidades como um todo. Assim, preocupo-me desde o aspecto visual do programa quanto aos aspectos mais técnicos, visando escrever o código da forma mais simples e clara.", 165); 
-  doc.text(resumo, 25, h+=5); 
- 
-  doc.setFontSize(13); 
-  doc.text("FORMAÇÃO", 23, h+=40); 
-  doc.rect(23, h, 165, 0.1, 'F'); 
-  doc.setFontSize(10); 
-  doc.text("Sistema de informação – PUCRS (2016 - cursando)", 25, h+=5); 
-  doc.text("Bacharelado em Educação Física e Ciências do Desporto - PUCRS (concluído 2015).", 25, h+=5); 
- 
-  doc.setFontSize(13); 
-  doc.text("EXPERIÊNCIA PROFISSIONAL", 23, h+=10); 
-  doc.rect(23, h, 165, 0.1, 'F'); 
-  doc.setFontSize(10); 
-  doc.text("06/16 - 02/18 – Gerência da Tecnologia da Informação e Telecomunicação - PUCRS", 25, h+=5); 
-  doc.text("Cargo: Programador", 25, h+=5); 
-  doc.setFontSize(10); 
-  doc.text("Principais atividades: Realizar manutenção e evolução dos sistemas já existentes, além de elaboração e " ,25, h+=5); 
-  doc.text("criação de novos. Utilizando principalmente Java como linguagem de programação back-end e os  " ,25, h+=5); 
-  doc.text("frameworks JSP, Servlet, Spring MVC, Hibernate. Como padrão era utilizado o banco de dados Oracle.",25, h+=5); 
-
-  doc.text("02/18 - atual – DBC Company", 25, h+=10); 
-  doc.text("Cargo: Programador", 25, h+=5); 
-  doc.setFontSize(10); 
-  doc.text("Principais atividades: Realizar a construção de sistemas utilizando Java como linguagem de programação" ,25, h+=5); 
-  doc.text("back-end e frameworks Primefaces e Hibernate, além de realizar as customizações necessárias utilizando " ,25, h+=5); 
-  doc.text("Bootstrap, JavaScript e JQuery.",25, h+=5); 
- 
-  doc.setFontSize(13); 
-  doc.text("QUALIFICAÇÕES E ATIVIDADES PROFISSIONAIS", 23, h+=10); 
-  doc.rect(23, h, 165, 0.1, 'F'); 
-  doc.setFontSize(10); 
-  doc.text("- HTML5 e CSS3 I: Suas primeiras páginas da Web (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- HTML5 e CSS3 II: Turbinando as suas páginas (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Java I: Primeiros passos (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Java II: Orientação a Objetos (Alura.com, 2015)", 25, h+=5); 
-  doc.text("- Java III: Principais APIs e bibliotecas (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Java 8: Tire proveito dos novos recursos da linguagem (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Java: Dominando as Collections (Alura.com, 2016);", 25, h+=5); 
-  doc.text("- Java e JDBC: Trabalhando com um banco de dados (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Eclipse: Produtividade Extrema na IDE com Java (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Design Patterns Java I: Boas práticas de programação (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- SQL Completo (Softblue, 2015);", 25, h+=5); 
-  doc.text("- MySQL I: Iniciando suas consultas (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- MySQL II: Consultas poderosas (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Oracle I: Aprenda SQL usando esse famoso banco de dados (Alura.com, 2015);", 25, h+=5); 
-  doc.addPage(); 
-  h = 30; 
-  doc.text("- Oracle II: Consultas Complexas (Alura.com, 2016);", 25, h+=5); 
-  doc.text("- Lean Startup: Primeiros passos da sua startup enxuta (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Métodos Ágeis: Introdução (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Refatorando na prática (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Clojure: Introdução a programação funcional (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- JavaScript : Programando na linguagem da web (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Redis I: Armazenando chaves e valores (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Redis II: Estruturas e recursos na sua base NoSQL (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- SQLServer I: desvendando o banco de dados (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- SQLServer II: Consultas poderosas (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- UML: Uma introdução à modelagem  (Alura.com, 2015);", 25, h+=5); 
-  doc.text("- Android I: Crie sua App fantástica com Android Studio (Alura.com, 2016);", 25, h+=5); 
-  doc.text("- Jogos com Android: Desenvolva seu próprio Flappy Bird (Alura.com, 2016);", 25, h+=5); 
-  doc.text("- Scrum: Agilidade em seu projeto (Alura.com, 2016);", 25, h+=5); 
-  doc.text("- Android I: Crie sua App fantástica no Eclipse (Alura.com, 2016);", 25, h+=5); 
-  doc.text("- Jogos com Android I: Desenvolvimento de jogos 2D (Alura.com, 2016);", 25, h+=5); 
-  doc.text("- Testes: TDD com Java (Alura.com, 2016);", 25, h+=5); 
-  doc.text("- Orientação a Objetos: Melhores técnicas com Java  (Alura.com, 2016);", 25, h+=5); 
-  doc.text("- Threads I : Programação paralela em Java  (Alura.com, 2016);", 25, h+=5); 
-  doc.text("- Desenvolvedor NodeJS e MongoDB. (Udemy, 2017);", 25, h+=5); 
-  doc.text("- Git Completo: Do Básico ao Avançado. (Udemy, 2017);", 25, h+=5); 
-  doc.text("- Introdução à Business Intelligence e Data Warehouse (BI do Brasil, 2018).", 25, h+=5); 
-  doc.text("- Treinamento Scrum Developer (DBCCompany, 2018);", 25, h+=5); 
-  doc.text("- Certificado CLF® (Certified Lean Inception Facilitator) – Lean Inception (Caroli.org, 2018);", 25, h+=5); 
-  doc.text("- Criar aplicativos com Ionic 3, Spring, OAuth2, REST e MongoDB (Udemy.com, 2018);", 25, h+=5); 
-
-  var today = new Date(); 
-  today.setHours(0, 0, 0, 0); 
-  doc.text(today.toLocaleDateString(), 175, h+=5); 
-  doc.save('CV_DanielMariaDaSilva.pdf'); 
-}
 
 function showOrHideEmail(){ 
   $("#emailtext").toggle("slow"); 
