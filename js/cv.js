76cb05f8c08bcb84e050e1e1e2bd2b701590ab5b
Daniel Maria da Silva <danielmariadasilva@gmail.com>
2019-04-21 01:09:59 -0300

    Ajustes do CV

--- js/cv.js
+++ js/cv.js
@@ -1,10 +1,10 @@
 var cursosList = [
     {title:"Android I: Crie sua App fantástica no Eclipse", date: new Date("03/22/2016"), duracao:720, showCV: false, showResume: false, company: "Alura.com"},
     {title:"Jogos com Android: Desenvolva seu próprio Flappy Bird", date: new Date("04/08/2016"), duracao:480, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Jogos com Android I: Desenvolvimento de jogos 2D", date: new Date("03/31/2016"), duracao:480, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Jogos com Android I: Desenvolvimento de jogos 2D", date: new Date("03/31/2016"), duracao:480, showCV: false, showResume: false, company: "Alura.com"},
     {title:"Android I: Crie sua App fantástica com Android Studio", date: new Date("03/11/2016"), duracao:600, showCV: true, showResume: false, company: "Alura.com"},
     {title:"Design Patterns Java I: Boas práticas de programação", date: new Date("10/29/2015"), duracao:1200, showCV: true, showResume: false, company: "Alura.com"},
-    {title:"Eclipse: Produtividade Extrema na IDE com Java", date: new Date("11/18/2015"), duracao:720, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Eclipse: Produtividade Extrema na IDE com Java", date: new Date("11/18/2015"), duracao:720, showCV: false, showResume: false, company: "Alura.com"},
     {title:"Orientação a Objetos: Melhores técnicas com Java", date: new Date("03/01/2016"), duracao:1200, showCV: false, showResume: false, company: "Alura.com"},
     {title:"Testes: TDD com Java", date: new Date("02/23/2016"), duracao:720, showCV: true, showResume: false, company: "Alura.com"},
     {title:"Java I: Primeiros passos", date: new Date("08/25/2015"), duracao:720, showCV: false, showResume: false, company: "Alura.com"},
@@ -14,9 +14,9 @@ var cursosList = [
     {title:"Java III: Principais APIs e bibliotecas", date: new Date("09/30/2015"), duracao:720, showCV: false, showResume: false, company: "Alura.com"},
     {title:"Java 8: Tire proveito dos novos recursos da linguagem", date: new Date("11/13/2015"), duracao:720, showCV: true, showResume: false, company: "Alura.com"},
     {title:"Clojure: Introdução a programação funcional", date: new Date("11/30/2015"), duracao:480, showCV: true, showResume: false, company: "Alura.com"},
-    {title:"Threads I : Programação paralela em Java", date: new Date("05/18/2016"), duracao:480, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"Threads I : Programação paralela em Java", date: new Date("05/18/2016"), duracao:480, showCV: false, showResume: false, company: "Alura.com"},
     {title:"Java: Dominando as Collections", date: new Date("08/01/2016"), duracao:1200, showCV: true, showResume: false, company: "Alura.com"},
-    {title:"JavaScript : Programando na linguagem da web", date: new Date("09/04/2015"), duracao:1200, showCV: true, showResume: false, company: "Alura.com"},
+    {title:"JavaScript : Programando na linguagem da web", date: new Date("09/04/2015"), duracao:1200, showCV: false, showResume: false, company: "Alura.com"},
     {title:"HTML5 e CSS3 I: Suas primeiras páginas da Web", date: new Date("09/01/2015"), duracao:1920, showCV: false, showResume: false, company: "Alura.com"},
     {title:"HTML5 e CSS3 II: Turbinando as suas páginas", date: new Date("10/15/2015"), duracao:2400, showCV: true, showResume: false, company: "Alura.com"},
     {title:"Métodos Ágeis: Introdução", date: new Date("11/13/2015"), duracao:720, showCV: false, showResume: false, company: "Alura.com"},
@@ -40,10 +40,11 @@ var cursosList = [
     {title:"Testes unitários em JAVA: Domine JUnit, Mockito e TDD", date: new Date("04/17/2019"), duracao:480, showCV: true, showResume: false,  company: "Udemy.com"},
     {title:"Treinamento Scrum Developer", date: new Date("01/01/2018"), duracao:480, showCV: true, showResume: false,  company: "DBCCompany"}, //
     {title:"Certificado CLF (Certified Lean Inception Facilitator", date: new Date("08/01/2018"), duracao:480, showCV: true, showResume: false, company: "Caroli.org"},
-    {title:"Introdução à Business Intelligence e Data Warehouse", date: new Date("02/14/2018"), duracao:300, showCV: false, showResume: false, company: "BI do Brasil"}
-  ];
+    {title:"Introdução à Business Intelligence e Data Warehouse", date: new Date("02/14/2018"), duracao:300, showCV: false, showResume: false, company: "BI do Brasil"},
+    {title:"Angular 7 e  integração de APIs", date: new Date("04/21/2019"), duracao:150, showCV: true, showResume: false, company: "Udemy.com"}
+];
 
-  Array.prototype.sumIgnoring = function (prop, ignore) {
+Array.prototype.sumIgnoring = function (prop, ignore) {
     var total = 0
     for ( var i = 0, _len = this.length; i < _len; i++ ) {
         if(!this[i][ignore]){
@@ -66,7 +67,7 @@ function printClasses(ignoreVar){
     getSortedCursos().forEach(element => {
         if(ignoreVar || element.showCV){
             htmlCursos += "<li>";
-            htmlCursos += element.title + ' (' + element.company + ', ' + element.date.getFullYear() + ');';
+            htmlCursos += element.title + '  - ' + Math.round(element.duracao/60) + 'h (' + element.company + ', ' + element.date.getFullYear() + ');';
             htmlCursos += "</li>";
         }
     });
@@ -74,10 +75,12 @@ function printClasses(ignoreVar){
     document.getElementById("cursos-qualificacoes").innerHTML = htmlCursos;
     if(!ignoreVar){
         let tempoCurso = cursosList.sumIgnoring('duracao', 'showCV');
-        document.getElementById("cursos-qualificacoes").innerHTML += "<p style='text-align: right;'> E mais " + Math.round(tempoCurso/60) + " horas em outros cursos.</p>";
+        let qtdCursos = cursosList.filter( curso => !curso.showCV).length
+        document.getElementById("cursos-qualificacoes").innerHTML += "<p style='text-align: right;'> E mais " + qtdCursos + " outros cursos. (" + Math.round(tempoCurso/60) + " horas)</p>";
     } else {
         let tempoCurso = cursosList.sumIgnoring('duracao', '');
-        document.getElementById("cursos-qualificacoes").innerHTML += "<p style='text-align: right;'> Total de " + Math.round(tempoCurso/60) + " horas.</p>";
+        let qtdCursos = cursosList.length
+        document.getElementById("cursos-qualificacoes").innerHTML += "<p style='text-align: right;'> Total de " + qtdCursos + " cursos. (" + Math.round(tempoCurso/60) +" horas) .</p>";
     }
 }
 
