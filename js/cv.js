70475dfcc0139f49aefcd8463485762921d4c348
Daniel <danielmariadasilva@gmail.com>
2020-11-19 18:49:34 -0300

    Incluindo a visualização dos cursos e tradução para inglês

--- js/cv.js
+++ js/cv.js
@@ -1,49 +1,52 @@
 var cursosList = [
-    {title:"Android I: Crie sua App fantástica no Eclipse", date: new Date("03/22/2016"), duracao:720, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Jogos com Android: Desenvolva seu próprio Flappy Bird", date: new Date("04/08/2016"), duracao:480, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Jogos com Android I: Desenvolvimento de jogos 2D", date: new Date("03/31/2016"), duracao:480, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Android I: Crie sua App fantástica com Android Studio", date: new Date("03/11/2016"), duracao:600, showCV: true, showResume: false, company: "Alura.com"},
-    {title:"Design Patterns Java I: Boas práticas de programação", date: new Date("10/29/2015"), duracao:1200, showCV: true, showResume: false, company: "Alura.com"},
-    {title:"Eclipse: Produtividade Extrema na IDE com Java", date: new Date("11/18/2015"), duracao:720, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Orientação a Objetos: Melhores técnicas com Java", date: new Date("03/01/2016"), duracao:1200, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Testes: TDD com Java", date: new Date("02/23/2016"), duracao:720, showCV: true, showResume: false, company: "Alura.com"},
-    {title:"Java I: Primeiros passos", date: new Date("08/25/2015"), duracao:720, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Java e JDBC: Trabalhando com um banco de dados", date: new Date("10/15/2015"), duracao:480, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Refatorando na prática", date: new Date("11/17/2015"), duracao:480, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Java II: Orientação a Objetos", date: new Date("08/28/2015"), duracao:720, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Java III: Principais APIs e bibliotecas", date: new Date("09/30/2015"), duracao:720, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Java 8: Tire proveito dos novos recursos da linguagem", date: new Date("11/13/2015"), duracao:720, showCV: true, showResume: false, company: "Alura.com"},
-    {title:"Clojure: Introdução a programação funcional", date: new Date("11/30/2015"), duracao:480, showCV: true, showResume: false, company: "Alura.com"},
-    {title:"Threads I : Programação paralela em Java", date: new Date("05/18/2016"), duracao:480, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Java: Dominando as Collections", date: new Date("08/01/2016"), duracao:1200, showCV: true, showResume: false, company: "Alura.com"},
-    {title:"JavaScript : Programando na linguagem da web", date: new Date("09/04/2015"), duracao:1200, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"HTML5 e CSS3 I: Suas primeiras páginas da Web", date: new Date("09/01/2015"), duracao:1920, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"HTML5 e CSS3 II: Turbinando as suas páginas", date: new Date("10/15/2015"), duracao:2400, showCV: true, showResume: false, company: "Alura.com"},
-    {title:"Métodos Ágeis: Introdução", date: new Date("11/13/2015"), duracao:720, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"UML Introdução: Modelagem de soluçõe", date: new Date("08/24/2015"), duracao: 480, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Scrum: Agilidade em seu projeto", date: new Date("01/14/2016"), duracao:600, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Lean Startup: Primeiros passos da sua startup enxuta", date: new Date("10/26/2015"), duracao:60, showCV: true, showResume: false, company: "Alura.com"},
-    {title:"MySQL II: Consultas poderosas", date: new Date("11/18/2015"), duracao:960, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"MySQL I: Iniciando suas consultas", date: new Date("09/12/2015"), duracao:480, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Oracle I: Aprenda SQL usando esse famoso banco de dados", date: new Date("12/01/2015"), duracao:480, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Redis I: Armazenando chaves e valores", date: new Date("11/25/2015"), duracao:1200, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Redis II: Estruturas e recursos na sua base NoSQL", date: new Date("11/27/2015"), duracao:600, showCV: true, showResume: false, company: "Alura.com"},
-    {title:"SQLServer I: desvendando o banco de dados", date: new Date("12/01/2015"), duracao:540, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"SQLServer II: Consultas poderosas", date: new Date("12/04/2015"), duracao:600, showCV: false, showResume: false, company: "Alura.com"},
-    {title:"Oracle II: Consultas Complexas", date: new Date("02/05/2016"), duracao:480, showCV: true, showResume: false, company: "Alura.com"},
-    {title:"SQL Completo", date: new Date("08/05/2015"), duracao:1200, showCV: false, showResume: false, company: "Softblue"},
-    {title:"Desenvolvedor NodeJS e MongoDB", date: new Date("07/27/2017"), duracao:930, showCV: true, showResume: false,  company: "Udemy.com"},
-    {title:"Git Completo: Do Básico ao Avançado", date: new Date("07/27/2017"), duracao:120, showCV: true, showResume: false,  company: "Udemy.com"},
-    {title:"Criar aplicativos com Ionic 3, Spring, OAuth2, REST e MongoDB", date: new Date("12/12/2018"), duracao:300, showCV: true, showResume: false,  company: "Udemy.com"},
-    {title:"Design Thinking com Crianças e Jovens: Aplicação e Exemplos", date: new Date("04/02/2019"), duracao:42, showCV: false, showResume: false,  company: "Udemy.com"},
-    {title:"API RESTful com Spring Boot e Java 8", date: new Date("04/06/2019"), duracao:930, showCV: true, showResume: false,  company: "Udemy.com"},
-    {title:"Testes unitários em JAVA: Domine JUnit, Mockito e TDD", date: new Date("04/17/2019"), duracao:480, showCV: true, showResume: false,  company: "Udemy.com"},
-    {title:"Treinamento Scrum Developer", date: new Date("01/01/2018"), duracao:480, showCV: true, showResume: false,  company: "DBCCompany"}, //
-    {title:"Certificado CLF (Certified Lean Inception Facilitator", date: new Date("08/01/2018"), duracao:480, showCV: true, showResume: false, company: "Caroli.org"},
-    {title:"Introdução à Business Intelligence e Data Warehouse", date: new Date("02/14/2018"), duracao:300, showCV: false, showResume: false, company: "BI do Brasil"},
-    {title:"Angular 7 e  integração de APIs", date: new Date("04/21/2019"), duracao:150, showCV: true, showResume: false, company: "Udemy.com"},
-    {title:"Docker: Ferramenta essencial para Desenvolvedores", date: new Date("05/02/2019"), duracao:330, showCV: true, showResume: false, company: "Udemy.com"},
-    {title:"Terminal Linux", date: new Date("03/23/2020"), duracao:60, showCV: true, showResume: false, company: "Udemy.com"}
+    {title:"Android I: Crie sua App fantástica no Eclipse", titleTransaled: "Android I: Create your fantastic App in Eclipse", date: new Date("03/22/2016"), duracao:720, showCV: false, company: "Alura.com", certification: "alura_android1eclipse"},
+    {title:"Jogos com Android: Desenvolva seu próprio Flappy Bird", titleTransaled: "Android games: Develop your own Flappy Bird", date: new Date("04/08/2016"), duracao:480, showCV: false, company: "Alura.com",  certification: "alura_fluppy"},
+    {title:"Jogos com Android I: Desenvolvimento de jogos 2D", titleTransaled: "Games with Android I: 2D game development", date: new Date("03/31/2016"), duracao:480, showCV: false, company: "Alura.com",  certification: "alura_androidjogo"},
+    {title:"Android I: Crie sua App fantástica com Android Studio", titleTransaled: "Android I: Create your fantastic App with Android Studio", date: new Date("03/11/2016"), duracao:600, showCV: true, company: "Alura.com",  certification: "alura_android1"},
+    {title:"Design Patterns Java I: Boas práticas de programação", titleTransaled: "Design Patterns Java I: Good programming practices", date: new Date("10/29/2015"), duracao:1200, showCV: true, company: "Alura.com",  certification: "alura_designpattern"},
+    {title:"Eclipse: Produtividade Extrema na IDE com Java", titleTransaled: "", date: new Date("11/18/2015"), duracao:720, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"Orientação a Objetos: Melhores técnicas com Java", titleTransaled: "", date: new Date("03/01/2016"), duracao:1200, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"Testes: TDD com Java", titleTransaled: "", date: new Date("02/23/2016"), duracao:720, showCV: true, company: "Alura.com",  certification: ""},
+    {title:"Java I: Primeiros passos", titleTransaled: "", date: new Date("08/25/2015"), duracao:720, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"Java e JDBC: Trabalhando com um banco de dados", titleTransaled: "", date: new Date("10/15/2015"), duracao:480, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"Refatorando na prática", titleTransaled: "", date: new Date("11/17/2015"), duracao:480, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"Java II: Orientação a Objetos", titleTransaled: "", date: new Date("08/28/2015"), duracao:720, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"Java III: Principais APIs e bibliotecas", titleTransaled: "", date: new Date("09/30/2015"), duracao:720, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"Java 8: Tire proveito dos novos recursos da linguagem", titleTransaled: "", date: new Date("11/13/2015"), duracao:720, showCV: true, company: "Alura.com",  certification: ""},
+    {title:"Clojure: Introdução a programação funcional", titleTransaled: "", date: new Date("11/30/2015"), duracao:480, showCV: true, company: "Alura.com",  certification: ""},
+    {title:"Threads I : Programação paralela em Java", titleTransaled: "", date: new Date("05/18/2016"), duracao:480, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"Java: Dominando as Collections", titleTransaled: "", date: new Date("08/01/2016"), duracao:1200, showCV: true, company: "Alura.com",  certification: ""},
+    {title:"JavaScript : Programando na linguagem da web", titleTransaled: "", date: new Date("09/04/2015"), duracao:1200, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"HTML5 e CSS3 I: Suas primeiras páginas da Web", titleTransaled: "", date: new Date("09/01/2015"), duracao:1920, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"HTML5 e CSS3 II: Turbinando as suas páginas", titleTransaled: "", date: new Date("10/15/2015"), duracao:2400, showCV: true, company: "Alura.com",  certification: ""},
+    {title:"Métodos Ágeis: Introdução", titleTransaled: "", date: new Date("11/13/2015"), duracao:720, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"UML Introdução: Modelagem de soluçõe", titleTransaled: "", date: new Date("08/24/2015"), duracao: 480, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"Scrum: Agilidade em seu projeto", titleTransaled: "", date: new Date("01/14/2016"), duracao:600, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"Lean Startup: Primeiros passos da sua startup enxuta", titleTransaled: "", date: new Date("10/26/2015"), duracao:60, showCV: true, company: "Alura.com",  certification: ""},
+    {title:"MySQL II: Consultas poderosas", titleTransaled: "", date: new Date("11/18/2015"), duracao:960, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"MySQL I: Iniciando suas consultas", titleTransaled: "", date: new Date("09/12/2015"), duracao:480, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"Oracle I: Aprenda SQL usando esse famoso banco de dados", titleTransaled: "", date: new Date("12/01/2015"), duracao:480, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"Redis I: Armazenando chaves e valores", titleTransaled: "", date: new Date("11/25/2015"), duracao:1200, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"Redis II: Estruturas e recursos na sua base NoSQL", titleTransaled: "", date: new Date("11/27/2015"), duracao:600, showCV: true, company: "Alura.com",  certification: ""},
+    {title:"SQLServer I: desvendando o banco de dados", titleTransaled: "", date: new Date("12/01/2015"), duracao:540, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"SQLServer II: Consultas poderosas", titleTransaled: "", date: new Date("12/04/2015"), duracao:600, showCV: false, company: "Alura.com",  certification: ""},
+    {title:"Oracle II: Consultas Complexas", titleTransaled: "", date: new Date("02/05/2016"), duracao:480, showCV: true, company: "Alura.com",  certification: ""},
+    {title:"SQL Completo", titleTransaled: "", date: new Date("08/05/2015"), duracao:1200, showCV: false, company: "Softblue",  certification: ""},
+    {title:"Desenvolvedor NodeJS e MongoDB", titleTransaled: "", date: new Date("07/27/2017"), duracao:930, showCV: true,  company: "Udemy.com",  certification: "nodejs"},
+    {title:"Git Completo: Do Básico ao Avançado", titleTransaled: "", date: new Date("07/27/2017"), duracao:120, showCV: true,  company: "Udemy.com",  certification: "git"},
+    {title:"Criar aplicativos com Ionic 3, Spring, OAuth2, REST e MongoDB", titleTransaled: "", date: new Date("12/12/2018"), duracao:300, showCV: true,  company: "Udemy.com",  certification: "ionic"},
+    {title:"Design Thinking com Crianças e Jovens: Aplicação e Exemplos", titleTransaled: "", date: new Date("04/02/2019"), duracao:42, showCV: false,  company: "Udemy.com",  certification: ""},
+    {title:"API RESTful com Spring Boot e Java 8", titleTransaled: "", date: new Date("04/06/2019"), duracao:930, showCV: true,  company: "Udemy.com",  certification: "api_restful"},
+    {title:"Testes unitários em JAVA: Domine JUnit, Mockito e TDD", titleTransaled: "", date: new Date("04/17/2019"), duracao:480, showCV: true,  company: "Udemy.com",  certification: "testes_unitarios"},
+    {title:"Treinamento Scrum Developer", titleTransaled: "", date: new Date("01/01/2018"), duracao:480, showCV: true,  company: "DBCCompany",  certification: ""},
+    {title:"Certificado CLF (Certified Lean Inception Facilitator", titleTransaled: "", date: new Date("08/01/2018"), duracao:480, showCV: true, company: "Caroli.org",  certification: "LeanInception"},
+    {title:"Introdução à Business Intelligence e Data Warehouse", titleTransaled: "", date: new Date("02/14/2018"), duracao:300, showCV: false, company: "BI do Brasil",  certification: "bida"},
+    {title:"Angular 7 e  integração de APIs", titleTransaled: "", date: new Date("04/21/2019"), duracao:150, showCV: true, company: "Udemy.com",  certification: "angular"},
+    {title:"Docker: Ferramenta essencial para Desenvolvedores", titleTransaled: "", date: new Date("05/02/2019"), duracao:330, showCV: true, company: "Udemy.com",  certification: "docker"},
+    {title:"Terminal Linux", titleTransaled: "Linux terminal", date: new Date("03/23/2020"), duracao:60, showCV: true, company: "Udemy.com",  certification: "terminal_linux"},
+    {title:"Apache Kafka and Spring Boot (Consumer, Producer)", titleTransaled: "", date: new Date("05/20/2020"), duracao:60, showCV: true, company: "Udemy.com",  certification: ""},
+    {title:"Reactive Programming with Spring Framework 5", titleTransaled: "", date: new Date("10/04/2020"), duracao:240, showCV: true, company: "Udemy.com",  certification: "webflux"},
+    {title:"Quarkus com Panache, GraalVM, GitlabCI e AWS", titleTransaled: "Quarkus with Panache, GraalVM, GitlabCI and AWS", date: new Date("07/08/2020"), duracao:90, showCV: true, company: "Udemy.com",  certification: "quarkus"}
 ];
 
 Array.prototype.sumIgnoring = function (prop, ignore) {
@@ -69,7 +72,7 @@ function printClasses(ignoreVar){
     getSortedCursos().forEach(element => {
         if(ignoreVar || element.showCV){
             htmlCursos += "<li>";
-            htmlCursos += element.title + '  - ' + Math.round(element.duracao/60) + 'h (' + element.company + ', ' + element.date.getFullYear() + ');';
+            htmlCursos += element.title + '  - ' + Math.round(element.duracao/60) + 'h (' + element.company + ', ' + element.date.getFullYear() + '); ' + buildExternalLink(element) + ' ' + transale(element) ;
             htmlCursos += "</li>";
         }
     });
@@ -84,6 +87,25 @@ function printClasses(ignoreVar){
         let qtdCursos = cursosList.length
         document.getElementById("cursos-qualificacoes").innerHTML += "<p style='text-align: right;'> Total de " + qtdCursos + " cursos. (" + Math.round(tempoCurso/60) +" horas) .</p>";
     }
+}
+
+function buildExternalLink(element) {
+    if(element.certification != ""){
+        return "<img src='./img/externo.svg' class='footer-button' onclick='showPDF(\"" + element.certification + "\")'>";        
+    }
+    return "";
+}
+
+
+function transale(element) {
+    if(element.titleTransaled != "") {
+        return '<img src=\"./img/reino-unido.svg\"/ class="footer-button" title="' + element.titleTransaled + '"> ';
+    }
+    return "";
+}
+
+function showPDF(certification){
+    window.open("./img/curses/" + certification + ".pdf","_blank");
 }
 
 function showAllClasses(){
--- js/language.js
+++ js/language.js
@@ -5,18 +5,29 @@ window.onload = function() {
 }
 
 function putTextInLabels(){
-    let apresentation, education;
+    let apresentation, education, experience_one_about, experience_one_info, experience_two_about, experience_two_info;
     if(lang === "PT"){
       apresentation = "Trabalho profissionalmente com programação de software desde 2016. Na maior parte do meu dia trabalho principalmente com backend Java, com o framework Spring. Já tive contato com outros frameworks, tais como PrimeFaces, JPA e JSF. Em meu trabalho utilizo na maior parte das vezes MongoDB para banco de dados, mas eventualmente utilizo Oracle. Busco aprimorar meus conhecimentos em programação e em desenvolvimento de software, aplicando minhas habilidades nos projetos e necessidades da empresa.";
       education = "<li>Sistema de informação – PUCRS (concluído 2020)</li> <li>Bacharelado em Educação Física e Ciências do Desporto - PUCRS (concluído 2015).</li>"
-    
+      experience_one_info = "<h2>DBC Company</h2> <p>Fev. 2018 - Atual</p> <p>Desenvolvedor</p>";
+      experience_one_about = "<b>Principais atividades: </b>Realizar a manutenção e construção de sistemas ao realizar o trabalho alocado no cliente como outsourcing . Durante os projetos em que participei atuei como programador back-end , utilizando, na maior parte do tempo, a linguagem Java, atuando também como desenvolvedor Dotnet. Utilizei Spring, Hibernate e Apache Camel na maior parte do trabalho. Também atuei como desenvolvedor front-end , utilizando Bootstrap, JavaScript, JQuery, Primefaces e eventualmente AngularJS e JavaFX.";
+      experience_two_info = "<h2>Pontifícia Universidade Católica do Rio Grande do Sul</h2> <p>Jun. 2016 - Fev. 2018</p> <p>Desenvolvedor</p>";
+      experience_two_about = "<b>Principais atividades: </b>Realizar manutenção e evolução dos sistemas já existentes, como também a elaboração e criação de novas soluções. Utilizava principalmente Java como linguagem de programação back-end, com o apoio de frameworks, tais como JSP, Servlet, Spring MVC, Hibernate. Realizava o controle de versionamento utilizando SVN e Oracle como o banco de dados das aplicações. Também, quando necessário, realizava  geração de relatórios e a manutenção do banco de dados.";
+ 
     } else {
       apresentation = "I have been working professionally with software programming since 2016. Most of my day I work mainly with Java backend, with the Spring framework. I have had contact with other frameworks, such as PrimeFaces, JPA and JSF. In my work I mostly use MongoDB for databases, but eventually I use Oracle. I seek to improve my knowledge in programming and software development, applying my skills to the company's projects and needs.";
       education = "<li>Information Systems – PUCRS (finished 2020)</li> <li>Physical Education - PUCRS (finished 2015).</li>"
-    
+      experience_one_info = "<h2>DBC Company</h2> <p>Feb 2018 - Today</p> <p>Software Developer</p>";
+      experience_one_about = experience_one_about_EN
+      experience_two_info = "<h2>Pontifical Catholic University of Rio Grande do Sul (PUCRS)</h2> <p>Jun 2016 - Feb 2018</p> <p>Software Developer</p>";
+      experience_two_about = experience_two_about_EN
     }
     document.getElementById("apresentation").innerHTML = apresentation;
     document.getElementById("education").innerHTML = education;
+    document.getElementById("experience_one_about").innerHTML = experience_one_about;
+    document.getElementById("experience_one_info").innerHTML = experience_one_info;
+    document.getElementById("experience_two_about").innerHTML = experience_two_about;
+    document.getElementById("experience_two_info").innerHTML = experience_two_info;
   }
   
   function changeEN(){
@@ -28,3 +39,29 @@ function putTextInLabels(){
     lang = "PT";
     putTextInLabels();
   }
+
+  var experience_one_about_EN = 
+  "<b>Outsourcing Sicredi (banking sector):</b> " + 
+    "<li> Development and maintenance of Java microservices. Java with Spring (MVC and Webflux) were used. All communication async was made using Kafka. Almost all microservices were built using MongoDB or PDB and, when needed the communication with Oracle and its routines were called; </li><li> Create and execute test cases in JUnit for unit testing with the support of SonarLint; </li>" + 
+    "<li> Participate in alignment demands and projects meetings; </li>" + 
+    "<li> Use of agile methodologies. </li>" + 
+    "<li> Use JIRA for trouble tickets and Confluence for our knowledge base;</li>" +
+    "<b> Outsourcing TNT Mercúrio (transport sector):</b> " + 
+    "<li>  Development and maintenance of Java systems (JPA [Hibernate], EJB, JSF, Spring Framework and JSP) using Oracle as database, Weblogic as application servers and SVN as version control tool;</li>" +
+    "<li>  Create and execute test cases in JUnit for unit testing;</li>" +
+    "<li>  Participate in alignment demands and projects meetings;</li>" +
+    "<li>  Use of agile methodologies.</li>" +
+"<b>  Outsourcing Sicredi (banking sector):</b>"  +
+    "<li>  For the backend, Java 8, Spring MVC and Hibernate were used.. For the frontend, PrimeFaces was used, and SCSS and Javascript to customize the interface. </li>" +
+    "<li>  Create and execute test cases in JUnit for unit testing;</li>" +
+    "<li>  Participate in alignment demands and project meetings; </li>" +
+    "<li>  Use of agile methodologies.</li>" +
+"<b>Intern project:</b>" +
+    "<li>  Development of an intern program to management and administration support;</li>" +
+    "<li>  Use of PrimeFaces, Hibernate and Spring;</li>" +
+    "<li>  Use of agile methodologies.</li>";
+  
+  var experience_two_about_EN = 
+  "<li>Analysis, development and maintenance of Java systems (JPA [Hibernate], EJB, JSF, RichFaces, Spring Framework, Servlets and JSP) using Oracle as database, JBoss and Tomcat as application servers, and SVN as version control tool;</li>" +
+  "<li>Analysis, development and maintenance of database routines with Oracle PL/SQL;</li>" +
+  "<li>Create and execute test cases in JUnit for unit testing.</li>";
